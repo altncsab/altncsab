2b443c91d2b21b6319c2a9ed18bc5d344fd2aa52
Csaba Nagy <csaba_nagy_flex@hotmail.com>
2024-01-23 10:34:50 +0100

    Introducing UsersGuide - About box - Users guide box - convert from markdown to html - updated pipeline - updated installer

---
+++ SqlScriptRunner/SqlScriptRunner/Forms/UsersGuideForm.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace SqlScriptRunner.Forms
+{
+    partial class UsersGuideForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.webBrowserUsersGuide = new System.Windows.Forms.WebBrowser();
+            this.SuspendLayout();
+            // 
+            // webBrowserUsersGuide
+            // 
+            this.webBrowserUsersGuide.AllowWebBrowserDrop = false;
+            this.webBrowserUsersGuide.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowserUsersGuide.IsWebBrowserContextMenuEnabled = false;
+            this.webBrowserUsersGuide.Location = new System.Drawing.Point(0, 0);
+            this.webBrowserUsersGuide.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowserUsersGuide.Name = "webBrowserUsersGuide";
+            this.webBrowserUsersGuide.Size = new System.Drawing.Size(800, 450);
+            this.webBrowserUsersGuide.TabIndex = 0;
+            this.webBrowserUsersGuide.Navigating += new System.Windows.Forms.WebBrowserNavigatingEventHandler(this.webBrowserUsersGuide_Navigating);
+            // 
+            // UsersGuideForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.webBrowserUsersGuide);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Name = "UsersGuideForm";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Users Guide";
+            this.Load += new System.EventHandler(this.UsersGuideForm_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowserUsersGuide;
+    }
+}
---
+++ SqlScriptRunner/SqlScriptRunner/Forms/UsersGuideForm.cs
@@ -0,0 +1,56 @@
+﻿using SqlScriptRunner.Extensions;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SqlScriptRunner.Forms
+{
+    public partial class UsersGuideForm : Form
+    {
+        string navigationPath;
+        public UsersGuideForm()
+        {
+            InitializeComponent();
+        }
+        public UsersGuideForm(string path) : this()
+        {
+            navigationPath = path;
+        }
+
+        private void UsersGuideForm_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                if (navigationPath != null)
+                {
+                    webBrowserUsersGuide.Navigate(navigationPath);
+                }
+            }
+            catch (Exception ex)
+            {
+
+                this.ShowErrorMessage(ex);
+            }
+        }
+
+        private void webBrowserUsersGuide_Navigating(object sender, WebBrowserNavigatingEventArgs e)
+        {
+            try
+            {
+                // TODO: Cancel the navigation if it would go outside we page
+                // MessageBox.Show(this, e.TargetFrameName, "Target Frame Name", MessageBoxButtons.OK, MessageBoxIcon.Information); // only temporary!
+            }
+            catch (Exception ex)
+            {
+
+                this.ShowErrorMessage(ex);
+            }
+        }
+    }
+}
